0ea1eba2245042f4a06356b4e5deb9e22b7d4424
Theo <gmbaxevanos@gmx.ch>
2021-08-12 17:02:27 +0200

    get all and get one done

--- server/index.js
+++ server/index.js
@@ -9,32 +9,55 @@ app.use(express.json()); // request.body
 
 // Routes (Schnittstellen)
 
-
 // create a todo
 
-app.post("/todos", async(request, response) => {
-    try {
-         //console.log(request.body); -> destructure that
+app.post("/todos", async (request, response) => {
+  try {
+    //console.log(request.body); -> destructure that
 
-        const { description } = request.body;
-        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
+    const { description } = request.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
 
-        response.json(newTodo.rows[0]);
-
-    } catch (error) {
-        console.log(error.message);
-    }
+    response.json(newTodo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
 });
 
 // get all todos
 
+app.get("/todos", async (request, response) => {
+  try {
+    const allToDos = await pool.query("SELECT * FROM todo");
+    response.json(allToDos.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
 // get one todo
+
+app.get("/todos/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    // 0 -> only the first item
+    response.json(todo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 // update a todo
 
 // delete a todo
 
 app.listen(5000, () => {
-    console.log("server has started on port 5000");
+  console.log("server has started on port 5000");
 });
-
